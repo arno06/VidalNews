deadac9ba8a273fca2cc1347873b39c570fd7a54
arno06 <arno06@gmail.com>
2017-12-10 17:59:49 +0100

    Finalisation des actus  + Layout et style de la liste  + Layout et style du détail  + Transition

--- app/views/news/list.js
+++ app/views/news/list.js
@@ -1,14 +1,20 @@
 
 var config = require('../../shared/config');
 var http = require('http');
+var he = require('he');
 
 var observableModule = require('data/observable');
 var frame = require("ui/frame");
+var dictionary = require('../../vidal/Dictionary');
 
 class NewsListViewModel extends observableModule.Observable{}
 
 var bc = new NewsListViewModel();
 
+function formatDate(pDate){
+    let d = pDate.split('-');
+    return d[2]+" "+dictionary.months[Number(d[1])-1]+" "+d[0];
+}
 
 function navigatingToHandler(args) {
     var page = args.object;
@@ -27,8 +33,17 @@ exports.tapHandler = function(e){
 
     frame.topmost().navigate({
         moduleName:'views/news/details',
+        transition:{
+            name: "slide",
+            duration: 380,
+            curve: "easeOut"
+        },
         bindingContext:news
     });
+};
+
+exports.showSideDrawer = function(){
+    console.log("open menu");
 };
 
 exports.navigatingToHandler = navigatingToHandler;
@@ -38,6 +53,13 @@ exports.loadedHandler = function(){
     var url = config.api.resources.url+"news?order=publication_date_news:desc&token="+config.api.resources.token;
 
     http.getJSON(url).then(function(r){
+        for(var i = 0, max = r.results.length; i<max; i++){
+            r.results[i].decoded_title = he.decode(r.results[i].title_news);
+            r.results[i].type.decoded_name = he.decode(r.results[i].type.name_type);
+            r.results[i].author.name_user = he.decode(r.results[i].author.name_user);
+            r.results[i].author.firstname_user = he.decode(r.results[i].author.firstname_user);
+            r.results[i].formated_date = formatDate(r.results[i].publication_date_news);
+        }
         bc.set('news', r.results);
     }, function(e){});
 };
